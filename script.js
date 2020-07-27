da7d5750ccfc5d179fdedeed899128a0927a8759
itamar mizrahi <yotheitamarmizrahi@gmail.com>
2020-07-28 00:17:07 +0300

    first commit

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 let k = document.getElementById("title");
-k.style.backgroundSize = 18;
-k.style.backgroundSize = 1;
+k.style.color="blue";
+window.localStorage.setItem('user', k.style.color);
+k.style.color="red";
+
+let display= () =>
+    {
+      console.log(window.localStorage.getItem('user'));    
+      k.innerHTML=window.localStorage.getItem('user');
+    }
+display(); 
+ 
