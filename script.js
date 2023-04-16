4c9a80a57961532033235d3829459b36c93d4ce9
Itamar Mizrahi <48559693+itamar-mizrahi@users.noreply.github.com>
2023-04-17 02:02:46 +0300

    init commit

--- script.js
+++ script.js
@@ -1,41 +1,48 @@
-(function(){
-  
-    var list = document.querySelector('#list'),
-        form = document.querySelector('form'),
-        item = document.querySelector('#item');
-    
-    form.addEventListener('submit',function(e){
+(function () {
+  var list = document.querySelector("#list"),
+    form = document.querySelector("form"),
+    item = document.querySelector("#item");
+
+  form.addEventListener(
+    "submit",
+    function (e) {
       e.preventDefault();
-      list.innerHTML += '<li>' + item.value + '</li>';
+      list.innerHTML += "<li>" + item.value + "</li>";
       store();
       item.value = "";
-    },false)
-    
-    list.addEventListener('click',function(e){
+    },
+    false
+  );
+
+  list.addEventListener(
+    "click",
+    function (e) {
       var t = e.target;
-      if(t.classList.contains('checked')){
+      if (t.classList.contains("checked")) {
         t.parentNode.removeChild(t);
       } else {
-        t.classList.add('checked');
+        t.classList.add("checked");
       }
       store();
-    },false)
-    
-    function store() {
-      window.localStorage.myitems = list.innerHTML;
+    },
+    false
+  );
+
+  function store() {
+    window.localStorage.myitems = list.innerHTML;
+  }
+
+  function getValues() {
+    var storedValues = window.localStorage.myitems;
+    if (!storedValues) {
+      list.innerHTML =
+        "<li>Make a to do list</li>" +
+        "<li>Check off first thing on the to do list</li>" +
+        "<li>Realize you have already accomplished 2 things in the list</li>" +
+        "<li>Reward yourself with a nap</li>";
+    } else {
+      list.innerHTML = storedValues;
     }
-    
-    function getValues() {
-      var storedValues = window.localStorage.myitems;
-      if(!storedValues) {
-        list.innerHTML = '<li>Make a to do list</li>'+
-                         '<li>Check off first thing on the to do list</li>'+
-                         '<li>Realize you have already accomplished 2 things in the list</li>'+
-                         '<li>Reward yourself with a nap</li>';
-      }
-      else {
-        list.innerHTML = storedValues;
-      }
-    }
-    getValues();
-  })();
+  }
+  getValues();
+})();
